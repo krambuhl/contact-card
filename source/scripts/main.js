e56c133d1e1b89ce0cc978673b63709d493d069c
Evan Krambuhl <thekrambuhl@gmail.com>
2015-01-21 17:49:23 -0800

    connection to ganglion

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,9 +1,23 @@
+var socket = require('socket.io-client')('http://localhost:4201');
 var ready = require('domready');
+var Bank = require('message-bank');
 
 var Contacts = require('./contacts');
 
-window.EApp = {};
+var kram = {
+  bank: new Bank()
+};
 
-ready(function() {
-  window.EApp.contacts = new Contacts('.l-contacts');
+kram.bank.subscribe('CONTACTS', function(data, opts) {
+  console.log(kram.bank._store);
 });
+
+
+socket.on('data', function(data) {
+  // console.log(data, kram.bank._store);
+  kram.bank.dispatch(data);
+});
+
+
+
+window.kram = kram;
